908c09a0bc8f06b3bb370ff156792e6708be7034
Fersho Pls <shaaf4ever@gmail.com>
2022-07-07 20:56:11 -0500

    Update Home.vue use padding instead margin

--- resources/js/Pages/Home/Home.vue
+++ resources/js/Pages/Home/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <ui-layout title="Home">
-    <div class="text-center pt-8 md:mt-12">
+    <div class="text-center pt-8 md:pt-12">
       <div class="text-3xl">🔥</div>
       <div class="text-2xl sm:text-3xl lg:text-4xl font-bold">Welcome.</div>
     </div>
